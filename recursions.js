c91a41556934f425b75a0fe72d81d0d933e6e73d
aniledev <elinamcgill@gmail.com>
2021-02-16 00:32:43 -0500

    FEAT: write comment code

--- recursions.js
+++ recursions.js
@@ -129,3 +129,13 @@ function fibonacci(number) {
 }
 
 console.log(fibonacci(11));
+
+/*
+7. Factorial
+Write a recursive function that finds the factorial of a given number. The factorial of a number can be found by multiplying that number by each number between itself and 1. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+Input : 5
+Output: 10
+Input: 7
+Output: 120
+*/
+
