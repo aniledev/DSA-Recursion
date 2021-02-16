a9258cbc334666e5da70d42eda4c835900fd8a83
aniledev <elinamcgill@gmail.com>
2021-02-16 00:10:17 -0500

    FEAT: write fibonacci recursive function

--- recursions.js
+++ recursions.js
@@ -112,3 +112,20 @@ function stringSplitter(string, symbol) {
   }
 }
 stringSplitter("03/12/1994", "/");
+
+/*
+6. Fibonacci
+Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+
+*/
+
+function fibonacci(number) {
+  // base case === 1 === 2
+  if (number === 1 || number === 2) {
+    return 1;
+  } else {
+    return fibonacci(number - 2) + fibonacci(number - 1);
+  }
+}
+
+console.log(fibonacci(11));
